380672c8d9d09b30c7601c7f94e646dbe6cae5db
Panoramacars <115028319+Panoramacars@users.noreply.github.com>
2022-11-08 15:28:18 +0400

    Update index.tsx

--- themes/amaranth/src/components/ArticleCard/index.tsx
+++ themes/amaranth/src/components/ArticleCard/index.tsx
@@ -3,7 +3,6 @@ import { getImage, IGatsbyImageData } from "gatsby-plugin-image";
 import { Types } from "gatsby-theme-advanced";
 
 import ReadButton from "./ArticleButton";
-import ArticleInfo from "../ArticleInfo";
 import { H3 } from "../../theme";
 import { TransparentLink } from "../Links";
 
@@ -34,7 +33,6 @@ const ArticleCard = ({ post, hero }: ArticleHeroCardProps): JSX.Element => {
       <S.Details hero={hero}>
         <S.Meta>
           <S.Header>
-            <ArticleInfo post={post} />
             <TransparentLink to={post.slug}>
               {/* Display as an H2 for accessibility and title semantics */}
               <H3 as="h2">{post.title}</H3>

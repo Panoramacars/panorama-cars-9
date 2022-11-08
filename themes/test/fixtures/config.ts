cd780e5d0ba8510153b5baa7cad77fd6f3fd2faa
Panoramacars <115028319+Panoramacars@users.noreply.github.com>
2022-11-08 16:20:48 +0400

    Update config.ts

--- themes/test/fixtures/config.ts
+++ themes/test/fixtures/config.ts
@@ -3,20 +3,20 @@ import { SiteConfig } from "../../advanced/src/config";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
+    title: "Panorama Rent a Car", // Homepage title
+    titleShort: "Panorama Cars", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "Panorama Rent a Car", // Website name used for homescreen (PWA) and SEO
     description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
     language: "en",
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
     fbAppId: "1111111111111111", // FB Application ID for using app insights
     twitterName: "Vagr9K", // Twitter handle of the website
-    url: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without the pathPrefix
+    url: "https://www.panoramacars.ae", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
     rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
 
     googleAnalyticsId: "UA-XXXXXXXX-A", // GA tracking ID
-    copyright: "© Copyright 2021 | Ruben Harutyunyan", // Copyright string for the footer of the website and RSS feed.
+    copyright: "© Copyright 2023 | Panorama Rent A Car L.L.C", // Copyright string for the footer of the website and RSS feed.
 
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
@@ -30,7 +30,7 @@ const config: SiteConfig = {
     twitterName: "Vagr9K", // Twitter username used for SEO
     linkedIn: "your-linkedin", // Used for contact information
     github: "vagr9k", // Used for contact information
-    email: "AdvancedUser@example.com", // Used for contact information and in the RSS feed
+    email: "info@panoramacars.ae", // Used for contact information and in the RSS feed
     location: "User Location", // User location used for SEO
     about: "A full-stack web developer looking for a challenge!", // User information used for the author section
     avatar: "https://i.pravatar.cc/300", // User avatar used for the author section
@@ -38,10 +38,10 @@ const config: SiteConfig = {
 
   // Organization information used for SEO
   organization: {
-    name: "Organization Name",
+    name: "Panorama Rent a Car",
     description: "Organization description",
     logoUrl: "/logos/logo-512.png",
-    url: "https://gatsby-advanced-starter-demo.netlify.com", // URL of the organization website
+    url: "https://www.panoramacars.ae", // URL of the organization website
   },
 
   // Gatsby Configuration

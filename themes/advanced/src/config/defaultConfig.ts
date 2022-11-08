0c37b503ff92e4cd570eefce1fb151b62b37dfc0
Panoramacars <115028319+Panoramacars@users.noreply.github.com>
2022-11-08 16:28:48 +0400

    Update defaultConfig.ts

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -3,16 +3,16 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
-    description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
+    title: "Panorama Cars", // Homepage title
+    titleShort: "Panorama Cars", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "Panorama Rent A Cars", // Website name used for homescreen (PWA) and SEO
+    description: "Panorama Rent A Cars, Abu Dhabi UAE.", // Website description used for RSS feeds/meta description tag
     language: "en", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
     url: "https://example.com", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
-    rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
+    rssTitle: "Panorama Cars RSS Feed", // Title of the RSS feed
+    copyright: "© Copyright 2023", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
   },
